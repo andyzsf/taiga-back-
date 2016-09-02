rodrigowebdeveloper/taiga-back

Save related stories in history items for epics

taiga/projects/history/freeze_impl.py
@@ -188,6 +188,18 @@ def _generic_extract(obj:object, fields:list, default=None) -> dict:
 
 
 @as_tuple
+def extract_user_stories(obj) -> list:
+    for user_story in obj.userstories.all():
+
+        yield {"id": user_story.id,
+               "ref": user_story.ref,
+               "subject": user_story.subject,
+               "project": {
+                   "id": user_story.project.id,
+                   "name": user_story.project.name,
+                   "slug": user_story.project.slug}}
+
+@as_tuple
 def extract_attachments(obj) -> list:
     for attach in obj.attachments.all():
         thumb_url = get_timeline_image_thumbnail_url(attach)
@@ -285,8 +297,6 @@ def epic_freezer(epic) -> dict:
         "ref": epic.ref,
         "owner": epic.owner_id,
         "status": epic.status.id if epic.status else None,
-        "is_closed": epic.is_closed,
-        "finish_date": str(epic.finish_date),
         "epics_order": epic.epics_order,
         "subject": epic.subject,
         "description": epic.description,
@@ -300,6 +310,7 @@ def epic_freezer(epic) -> dict:
         "blocked_note": epic.blocked_note,
         "blocked_note_html": mdrender(epic.project, epic.blocked_note),
         "custom_attributes": extract_epic_custom_attributes(epic),
+        "user_stories": extract_user_stories(epic),
     }
 
     return snapshot

andyzsf/taiga-back-

taiga/projects/history/freeze_impl.py
@@ -188,6 +188,18 @@ def _generic_extract(obj:object, fields:list, default=None) -> dict:
 
 
 @as_tuple
+def extract_user_stories(obj) -> list:
+    for user_story in obj.user_stories.all():
+
+        yield {"id": user_story.id,
+               "ref": user_story.ref,
+               "subject": user_story.subject,
+               "project": {
+                   "id": user_story.project.id,
+                   "name": user_story.project.name,
+                   "slug": user_story.project.slug}}
+
+@as_tuple
 def extract_attachments(obj) -> list:
     for attach in obj.attachments.all():
         thumb_url = get_timeline_image_thumbnail_url(attach)
@@ -285,8 +297,6 @@ def epic_freezer(epic) -> dict:
         "ref": epic.ref,
         "owner": epic.owner_id,
         "status": epic.status.id if epic.status else None,
-        "is_closed": epic.is_closed,
-        "finish_date": str(epic.finish_date),
         "epics_order": epic.epics_order,
         "subject": epic.subject,
         "description": epic.description,
@@ -300,6 +310,7 @@ def epic_freezer(epic) -> dict:
         "blocked_note": epic.blocked_note,
         "blocked_note_html": mdrender(epic.project, epic.blocked_note),
         "custom_attributes": extract_epic_custom_attributes(epic),
+        "user_stories": extract_user_stories(epic),
     }
 
     return snapshot